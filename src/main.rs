77537fb5ab9d492ce2ad89ba60486eb84cae499c
13Chrollo13 <shu_UwU@proton.me>
2026-07-09 14:12:10 +0200

    add all arrays to print function add player and ray and collums

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,32 @@ use std::io;
 
 
 fn main() {
+    let mut runden_score = 1;
     let mut line1 = ['x', 'x', 'x'];
     let mut line2 = ['x', 'x', 'x'];
     let mut line3 = ['x', 'z', 'x'];
-    /*
-    let mut line1 = ['x', 'x', 'x'];
-    let mut line2 = ["x ", "x ", "x "];
-    let mut line3 = ["x ", "x ", "x "];
 
-     */
     //println!("   1 2 3 \n 1 x x x \n 2 x x x  \n 3 x x x ");
 
-    //let line_fn = line1.clone();
     loop {
-        let runde = userinput_i32();
+        runden_score += 1;
+        if runden_score %2 == 0 {
+            println!("player 1 ist dran")
+        }
+        else {
+            println!("player 2 ist fran")
+        }
+        println!("gib deine zeile an");// welcher array
+        let zeile = userinput_i32();
+
+        println!("gib deine spalte ein");//wo im array
+        let spalte = userinput_i32();
         print_field(line1); //   print_field(line1: Vec<>); das Vec braucht man nich
         print_field(line2);
         print_field(line3);
-        break
+        if runden_score == 0 {
+            break
+        }
     }
 }
 fn print_field(line1: [char; 3]){

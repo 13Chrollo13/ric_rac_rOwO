e2da67ba84974475755e4414211ff29cd99417a3
13Chrollo13 <shu_UwU@proton.me>
2026-07-10 07:53:51 +0200

    add buchstaben push check if roe and colloums already contains the place UwU

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,46 @@ fn main() {
     let mut runden_score = 1;
     let mut line1 = ['x', 'x', 'x'];
     let mut line2 = ['x', 'x', 'x'];
-    let mut line3 = ['x', 'z', 'x'];
-
+    let mut line3 = ['x', 'x', 'x'];
+    let mut vec_roe:Vec<i32> = vec![];
+    let mut vec_collum:Vec<i32> = vec![];
     //println!("   1 2 3 \n 1 x x x \n 2 x x x  \n 3 x x x ");
 
     loop {
+        let mut buchstabe = 'a';
         runden_score += 1;
         if runden_score %2 == 0 {
-            println!("player 1 ist dran")
+            println!("player 1 ist dran");
+            buchstabe = 'U';
         }
         else {
-            println!("player 2 ist fran")
+            println!("player 2 ist dran");
+            buchstabe = 'w';
         }
+
         println!("gib deine zeile an");// welcher array
-        let zeile = userinput_i32();
+        let mut zeile = userinput_i32();
 
         println!("gib deine spalte ein");//wo im array
         let spalte = userinput_i32();
+
+        if vec_roe.contains(&zeile) && vec_collum.contains(&spalte) {
+            println!("das existiert schom");
+            zeile = 3;
+        }
+
+        vec_collum.push(spalte);
+        vec_roe.push(zeile);
+
+        let spalte_usize = spalte as usize;
+        match zeile {
+            0 => line1[spalte_usize] = buchstabe,
+            1 => line2[spalte_usize] = buchstabe,
+            2 => line3[spalte_usize] = buchstabe,
+            _=> runden_score -= 1
+        };
+
+
         print_field(line1); //   print_field(line1: Vec<>); das Vec braucht man nich
         print_field(line2);
         print_field(line3);
@@ -43,4 +66,7 @@ fn userinput_i32() -> i32 {
     let a = a.trim();
     let userinput: i32 = a.parse().unwrap();
     userinput
+}
+fn wincondition() {
+
 }

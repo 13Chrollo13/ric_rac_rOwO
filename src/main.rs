62a79ae8a4322df0c4bb1939d9b0f1bb3dd579cf
13Chrollo13 <shu_UwU@proton.me>
2026-07-16 10:19:58 +0200

    refaktor code to use a 3d array also rewrite match funktion and print function

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use std::io;
 
 fn main() {
     let mut runden_score = 1;
+    let mut line:[[i32; 3]; 3] = [[3i32; 3]; 3];
+    //[[3, 3, 3], [3, 3, 3], [3, 3, 3]]
+    println!("{:?}", line);
+    /*
     let mut line1 = ['x', 'x', 'x'];
     let mut line2 = ['x', 'x', 'x'];
     let mut line3 = ['x', 'x', 'x'];
+    let mut wincondition_array = [1,2,3];
+     */
 
     let mut exist_check:HashMap<i32, i32> = HashMap::new();
 
@@ -14,20 +20,19 @@ fn main() {
     /*
     let mut vec_roe:Vec<i32> = vec![];
     let mut vec_collum:Vec<i32> = vec![];
-
      */
     //println!("   1 2 3 \n 1 x x x \n 2 x x x  \n 3 x x x ");
 
     loop {
-        let mut buchstabe = 'a';
+        let mut zahl = 3;
         runden_score += 1;
         if runden_score %2 == 0 {
             println!("player 1 ist dran");
-            buchstabe = 'U';
+            zahl = 1;
         }
         else {
             println!("player 2 ist dran");
-            buchstabe = 'w';
+            zahl = 2;
         }
 
         println!("gib deine zeile an");// welcher array
@@ -47,7 +52,18 @@ fn main() {
 
         exist_check.insert(spalte, zeile);
 
+        println!("{}",line[1][2]);
         let spalte_usize = spalte as usize;
+
+        match zeile {
+            0 => line[0][spalte_usize] = zahl,
+            1 => line[1][spalte_usize] = zahl,
+            2 => line[2][spalte_usize] = zahl,
+            _=> runden_score -= 1
+        }
+        print_field(line);
+
+        /*
         match zeile {
             0 => line1[spalte_usize] = buchstabe,
             1 => line2[spalte_usize] = buchstabe,
@@ -56,25 +72,28 @@ fn main() {
         };
 
 
-        print_field(line1); //   print_field(line1: Vec<>); das Vec braucht man nich
-        print_field(line2);
-        print_field(line3);
+        let bit1 = print_field(line); //   print_field(line1: Vec<>); das Vec braucht man nich
+        let bit2 = print_field(line2);
+        let bit3 = print_field(line3);
+
+         */
+
+
         if runden_score == 0 {
             break
         }
+        //let a = wincondition(bit1, bit2, bit3);
     }
 }
-fn print_field(line1: [char; 3]){
-    let mut bit_array = 0;
-    for line1 in &line1{
-        print!(" {}", line1);
-        let x :u32 = line1.clone() as u32;
-        bit_array += x;
+fn print_field(line: [[i32; 3]; 3]){
+    let mut for_runde = 0;
+    for line in &line{
+        print!(" {}", line[for_runde]);
+        for_runde += 1;
     }
-
-    print!(" {}", bit_array);
     print!("\n")
 }
+
 
 fn userinput_i32() -> i32 {
     let mut a = String::new();
@@ -83,6 +102,17 @@ fn userinput_i32() -> i32 {
     let userinput: i32 = a.parse().unwrap();
     userinput
 }
-fn wincondition() {
 
+//todo!("use a 3d array instead of three different")
+
+
+/*
+fn wincondition(bit1: i32, bit2: i32,bit3: i32) -> i32 {
+    if bit1 == 255 || bit1 == 357 || bit2 == 255 || bit2 == 357 || bit3 == 255 || bit3 == 357 {
+        1
+    }
 }
+ U U U 255
+ w w w 357
+ x x x 360
+ */
